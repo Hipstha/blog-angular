17730e59ef99d8e770b87cbe0031579003ed5220
Alejandro Cruz <alex_crz97@hotmail.com>
2020-11-10 01:01:30 -0600

    Interfaz gráfica no responsiva

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostDetailComponent } from './pages/post-detail/post-detail.component';
+import { PostsComponent } from './pages/posts/posts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PostsComponent },
+  { path: 'posts', component: PostsComponent },
+  { path: 'posts/:id', component: PostDetailComponent },
+  { path: '**', redirectTo: 'posts'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { CategorySelectorComponent } from './components/category-selector/category-selector.component';
+import { PostsComponent } from './pages/posts/posts.component';
+import { PostDetailComponent } from './pages/post-detail/post-detail.component';
+import { CommentsComponent } from './components/comments/comments.component';
+import { CommentBoxComponent } from './components/comment-box/comment-box.component';
+import { PostComponent } from './components/post/post.component';
+import { AddPostComponent } from './components/add-post/add-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CategorySelectorComponent,
+    PostsComponent,
+    PostDetailComponent,
+    CommentsComponent,
+    CommentBoxComponent,
+    PostComponent,
+    AddPostComponent
   ],
   imports: [
     BrowserModule,
